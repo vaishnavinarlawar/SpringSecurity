a4039eaff7e712ca6edb8609205bdcf230cf0b5f
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2023-12-30 16:27:21 -0800

    Frsit Time Push COde

---
+++ src/main/java/com/fusiontech/controller/StudentController.java
@@ -0,0 +1,25 @@
+package com.fusiontech.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
+public class StudentController {
+	
+	
+	public StudentController() {
+		
+		System.out.println("inside StudentController const");
+	}
+	
+	
+	@GetMapping("/hii")
+	public String getname()
+	
+	{
+		System.out.println("inside getname");
+		return "hello java";
+	}
+
+}
