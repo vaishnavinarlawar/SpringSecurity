20f8bb13472c7143c04f2593f20a5a1498e95bf5
vaishnavinarlawar <vaishnavinarlawar27@gmail.com>
2024-03-06 19:37:11 -0800

    pushing spring security code

--- src/main/java/com/fusiontech/controller/StudentController.java
+++ src/main/java/com/fusiontech/controller/StudentController.java
@@ -6,17 +6,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 
 public class StudentController {
-	
-	
+
 	public StudentController() {
-		
+
 		System.out.println("inside StudentController const");
 	}
-	
-	
+
 	@GetMapping("/hii")
 	public String getname()
-	
+
 	{
 		System.out.println("inside getname");
 		return "hello java";
